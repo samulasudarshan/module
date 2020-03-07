1ba6a2d00aec5daa4eb70f3e11c2725080bdac17
samulasudarshan <samulasudarshan@gmail.com>
2020-03-07 11:29:15 +0530

    commit-1

--- prime.py
+++ prime.py
@@ -1,7 +1,7 @@
-import pdb
+#import pdb
 number1 = int(input("Enter starting number ")) 
 number2 = int(input("Enter ending number ")) 
-pdb.set_trace()
+#pdb.set_trace()
 for number in range(number1,number2+1): 
     is_prime = True
     for index in range(2,number//2+1): 
